34ccb626ee1fd6496fd0c032f8dbf53a13137b52
leoamaro01 <leoamaro2001@gmail.com>
2022-05-12 16:46:44 -0400

    Added PromptParseError

--- errors.py
+++ errors.py
@@ -6,3 +6,13 @@ class MenuParseError(Exception):
 
     def __str__(self) -> str:
         return f"Error on parsing menu: {self.menu}\n{self.message}"
+
+
+class PromptParseError(Exception):
+    def __init__(self, prompt: str, message: str) -> None:
+        self.prompt = prompt
+        self.message = message
+        super().__init__(message)
+
+    def __str__(self) -> str:
+        return f"Error on parsing prompt: {self.prompt}\n{self.message}"
